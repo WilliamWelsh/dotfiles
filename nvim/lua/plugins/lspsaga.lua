ac4f82ea008e3c205a1293a6010f69465ac8a2eb
William <WilliamWelsh@users.noreply.github.com>
2023-01-19 23:13:35 -0600

    Update lspsaga UI

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -6,6 +6,49 @@ return {
       server_filetype_map = {
         typescript = "typescript",
       },
+      ui = {
+        -- currently only round theme
+        theme = "round",
+        -- this option only work in neovim 0.9
+        title = true,
+        -- border type can be single,double,rounded,solid,shadow.
+        border = "solid",
+        winblend = 0,
+        expand = "",
+        collapse = "",
+        preview = " ",
+        code_action = "💡",
+        diagnostic = "🐞",
+        incoming = " ",
+        outgoing = " ",
+        colors = {
+          --float window normal background color
+          normal_bg = "#1D1F21",
+          --title background color
+          title_bg = "#afd700",
+          red = "#e95678",
+          magenta = "#b33076",
+          orange = "#FF8700",
+          yellow = "#f7bb3b",
+          green = "#afd700",
+          cyan = "#36d0e0",
+          blue = "#61afef",
+          purple = "#CBA6F7",
+          white = "#d1d4cf",
+          black = "#1c1c19",
+        },
+        kind = {},
+      },
+
+      symbol_in_winbar = {
+        enable = false,
+        separator = " ",
+        hide_keyword = true,
+        show_file = true,
+        folder_level = 2,
+        respect_root = false,
+        color_mode = true,
+      },
     })
 
     local opts = {
@@ -30,6 +73,7 @@ return {
 
     -- I use CMD + . (iTerm)
     vim.keymap.set("n", ";.", "<Cmd>Lspsaga code_action<CR>", opts)
+
     vim.keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
   end,
 }
