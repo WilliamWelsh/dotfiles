b41b2bee388219e1c929b21f0144539d4491b82d
William <WilliamWelsh@users.noreply.github.com>
2023-04-07 16:19:58 -0500

    Add apple music plugin

---
+++ nvim/lua/plugins/applemusic.lua
@@ -0,0 +1,12 @@
+return {
+	"srishanbhattarai/neovim-spotify",
+	cmd = {
+		"AppleMusicCurrentSong",
+		"AppleMusicNext",
+		"AppleMusicPrevious",
+		"AppleMusicPlay",
+		"AppleMusicPause",
+		"AppleMusicLyrics",
+	},
+	build = "bash install.sh",
+}
