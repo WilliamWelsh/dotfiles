3c9d0782de6733af951e04374128c100ad90a8c4
William <WilliamWelsh@users.noreply.github.com>
2023-09-09 17:06:33 -0500

    Add zigfmt & lint nullls

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,60 +1,62 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	event = "VeryLazy",
-	config = function()
-		local augroup_format = vim.api.nvim_create_augroup("Format", {
-			clear = true,
-		})
+  "jose-elias-alvarez/null-ls.nvim",
+  event = "VeryLazy",
+  config = function()
+    local augroup_format = vim.api.nvim_create_augroup("Format", {
+      clear = true,
+    })
 
-		local null_ls = require("null-ls")
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.prettierd.with({
-					filetypes = {
-						"javascript",
-						"javascriptreact",
-						"typescript",
-						"typescriptreact",
-						"tsx",
-						"vue",
-						"css",
-						"scss",
-						"less",
-						"html",
-						"json",
-						"jsonc",
-						"yaml",
-						"markdown",
-						"markdown.mdx",
-						"graphql",
-						"prisma",
-						"astro",
-					},
-				}),
-				null_ls.builtins.formatting.rustfmt,
-				-- null_ls.builtins.diagnostics.fish,
-				null_ls.builtins.formatting.stylua,
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.prettierd.with({
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "tsx",
+            "vue",
+            "css",
+            "scss",
+            "less",
+            "html",
+            "json",
+            "jsonc",
+            "yaml",
+            "markdown",
+            "markdown.mdx",
+            "graphql",
+            "prisma",
+            "astro",
+          },
+        }),
+        null_ls.builtins.formatting.rustfmt,
+        -- null_ls.builtins.diagnostics.fish,
+        null_ls.builtins.formatting.stylua,
 
-				null_ls.builtins.formatting.gofmt,
-				null_ls.builtins.formatting.swiftlint.with({ filetypes = { "swift" } }),
-				-- null_ls.builtins.diagnostics.golangci_lint,
-			},
-			on_attach = function(client, bufnr)
-				if client.server_capabilities.documentFormattingProvider then
-					vim.api.nvim_clear_autocmds({
-						buffer = 0,
-						group = augroup_format,
-					})
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup_format,
-						buffer = 0,
-						callback = function()
-							vim.lsp.buf.format()
-						end,
-					})
-				end
-			end,
-		})
-	end,
+        null_ls.builtins.formatting.zigfmt,
+
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.swiftlint.with({ filetypes = { "swift" } }),
+        -- null_ls.builtins.diagnostics.golangci_lint,
+      },
+      on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_clear_autocmds({
+            buffer = 0,
+            group = augroup_format,
+          })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup_format,
+            buffer = 0,
+            callback = function()
+              vim.lsp.buf.format()
+            end,
+          })
+        end
+      end,
+    })
+  end,
 }
