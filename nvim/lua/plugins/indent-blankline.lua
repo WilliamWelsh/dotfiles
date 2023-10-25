e03f90cbf4984560c72ab9fd69bc46fde83ca15d
William <WilliamWelsh@users.noreply.github.com>
2023-10-25 10:41:25 -0500

    Update indent-blankline for v3

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,18 +1,17 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  lazy = false,
-  config = function()
-    require("indent_blankline").setup({
-      char = "│",
-      show_trailing_blankline_indent = false,
-      show_first_indent_level = true,
-      show_current_context = false,
-      show_current_context_start = false,
-      space_char_blankline = " ",
-      use_treesitter = true,
-      context_patterns = { "class", "function", "method" },
-      filetype_exclude = { "help", "packer", "nvimtree", "dashboard", "neo-tree" },
-      buftype_exclude = { "terminal", "nofile", "quickfix" },
-    })
-  end,
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {
+		indent = {
+			char = "│",
+		},
+		scope = { enabled = false },
+		whitespace = {
+			remove_blankline_trail = true,
+		},
+		exclude = {
+			buftypes = { "terminal", "nofile", "quickfix" },
+			filetypes = { "help", "packer", "nvimtree", "dashboard", "neo-tree" },
+		},
+	},
 }
