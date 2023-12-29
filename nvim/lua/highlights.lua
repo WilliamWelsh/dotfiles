34d720dd9fef88f93cb7b554e18e65b478dae468
William <WilliamWelsh@users.noreply.github.com>
2023-12-29 10:49:30 -0600

    Make zen plugin bg transparent

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -4,57 +4,58 @@ vim.opt.wildoptions = "pum"
 vim.opt.pumblend = 0
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
-
-vim.g.neovide_transparency = 0.8
 vim.g.transparency = 0.8
 
 vim.api.nvim_set_hl(0, "WinSeparator", {
-	bg = "NONE",
-	fg = "#3F434B",
+  bg = "NONE",
+  fg = "#3F434B",
 })
 
 local hl_groups = {
-	-- Safe
-	"Normal",
-	"SignColumn",
-	"CursorLineNr",
-	"CursorLine",
+  -- Safe
+  "Normal",
+  "SignColumn",
+  "CursorLineNr",
+  "CursorLine",
 
-	-- Bad bois
-	"NormalNC",
+  -- Bad bois
+  "NormalNC",
 
-	-- Unknown
-	"NonText",
-	"Folded",
-	"SpecialKey",
-	"VertSplit",
-	"LineNr",
-	"NonText",
-	"EndOfBuffer",
-	"TabLineFill",
+  -- Unknown
+  "NonText",
+  "Folded",
+  "SpecialKey",
+  "VertSplit",
+  "LineNr",
+  "NonText",
+  "EndOfBuffer",
+  "TabLineFill",
+
+  -- Zen Plugin
+  "ZenBg",
 }
 
 for _, hl_group in ipairs(hl_groups) do
-	vim.api.nvim_set_hl(0, hl_group, {
-		bg = "NONE",
-	})
+  vim.api.nvim_set_hl(0, hl_group, {
+    bg = "NONE",
+  })
 end
 
 vim.api.nvim_set_hl(0, "HighlightUndo", {
-	bg = "red",
+  bg = "red",
 })
 
 vim.api.nvim_set_hl(0, "GitSignsAdd", {
-	bg = "NONE",
-	fg = "#348c37",
+  bg = "NONE",
+  fg = "#348c37",
 })
 
 vim.api.nvim_set_hl(0, "GitSignsDelete", {
-	bg = "NONE",
-	fg = "red",
+  bg = "NONE",
+  fg = "red",
 })
 
 vim.api.nvim_set_hl(0, "GitSignsChange", {
-	bg = "NONE",
-	fg = "#DE935F",
+  bg = "NONE",
+  fg = "#DE935F",
 })
