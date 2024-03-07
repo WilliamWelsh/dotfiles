dad341a89563f1c63d02d4cd1f336b239954ceca
William <WilliamWelsh@users.noreply.github.com>
2024-03-07 10:29:08 -0600

    Update nvimmaps

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -82,3 +82,13 @@ keymap.set("n", "<C-s>", ":w<CR>")
 keymap.set("i", "<C-s>", "<Esc>:w<CR>")
 keymap.set("i", "<C-S-s>", "<Esc>:wa<CR>")
 keymap.set("n", "<C-S-s>", ":wa<CR>")
+
+-- vim.api.nvim_set_keymap("n", "<Up>", "<Nop>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<Down>", "<Nop>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<Left>", "<Nop>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<Right>", "<Nop>", { noremap = true, silent = true })
+--
+-- vim.api.nvim_set_keymap("i", "<Up>", "<Nop>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("i", "<Down>", "<Nop>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("i", "<Left>", "<Nop>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("i", "<Right>", "<Nop>", { noremap = true, silent = true })
